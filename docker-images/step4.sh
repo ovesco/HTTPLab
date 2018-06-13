ce9dad22521a9f475692b43b0ce8149e6103589c
sysmoh <guillaume.hochet@gmail.com>
2018-06-13 07:24:33 +0200

    step4 shell

---
+++ docker-images/step4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+
+docker build -t res/apache_php ./apache-php/
+docker build -t res/express_dynamic ./express-dynamic/
+docker build -t res/apache_reverse_proxy ./apache-reverse-proxy/
+
+docker run -d res/apache_php
+docker run -d res/express_dynamic
+docker run -d -p 8080:80 res/apache_reverse_proxy
